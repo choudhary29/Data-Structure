216596b6bfcb9e0b28407057514e16d43708708b
choudhary29 <113602285+choudhary29@users.noreply.github.com>
2022-12-08 10:18:02 +0530

    dssd

---
+++ linked list/linked_list_split.cpp
@@ -0,0 +1,14 @@
+#include "ll.h"
+int main(){
+    struct node *start=NULL;
+    return 0;
+}
+struct node *start=NULL;
+    insbeg(&start,2);
+    insbeg(&start,3);
+    insbeg(&start,4);
+    insbeg(&start,5);
+    insbeg(&start,6);
+    insbeg(&start,7);
+    traverse(&start);
+    return 0;
